cee51507fe1672920774ea3deb67e35f2eeb112c
Justin Carboneau <carboneauj@gmail.com>
2025-01-03 18:02:56 -0500

    Update triviaData.js

--- utils/triviaData.js
+++ utils/triviaData.js
@@ -2,15 +2,10 @@ async function fetchTriviaQuestions() {
     try {
         const date = new Date().toISOString().split('T')[0];
         // const response = await trickleListObjects('trivia-questions', 1, true);
-        fetch('./questions/2025-01-04-questions-and-answers.json')
-          .then(response => response.json())
-          .then(data => {
-            console.log(data.questions, data.questions.length);
-            if (data.questions && data.questions.length > 0) {
-                return data.questions;
-            }              
-          })
-          .catch(error => console.error('Error loading JSON:', error));
+        const response = fetch('./questions/2025-01-04-questions-and-answers.json');
+        const data = await response.json();
+        console.log(data.questions, data.questions.length);
+        return data.questions;
     } catch (error) {
         reportError(error);
         return [];
