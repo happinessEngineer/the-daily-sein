ff45711308d64de78bdd8d8693806d9d88e36e0b
Justin Carboneau <justin.carboneau@carrot.co>
2025-01-05 16:22:34 -0500

    fix date/tz issue

--- utils/triviaData.js
+++ utils/triviaData.js
@@ -1,7 +1,10 @@
 async function fetchTriviaQuestions() {
     try {
-        const date = new Date(new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })).toISOString().split('T')[0];
-        const response = await fetch(`./questions/${date}-questions-and-answers.json`);
+        const date = new Date().toLocaleString('en-US', { timeZone: 'America/New_York' });
+        const [m, d, y] = date.split(',')[0].split('/');
+        const formattedDate = `${y.trim()}-${m.padStart(2, '0')}-${d.padStart(2, '0')}`;
+
+        const response = await fetch(`./questions/${formattedDate}-questions-and-answers.json`);
         const data = await response.json();
         return [data.gameNumber, data.questions];
     } catch (error) {
