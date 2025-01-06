fd79b74f8902f7f8a6b8072d1faf9bc97447b492
Justin Carboneau <justin.carboneau@carrot.co>
2025-01-06 12:23:58 -0500

    add character images to buttons, remove button hover

--- components/AnswerButton.js
+++ components/AnswerButton.js
@@ -1,5 +1,5 @@
 function AnswerButton({ character, isCorrect, isSelected, onClick, showResult }) {
-    let buttonClass = "answer-button w-full p-4 text-white rounded-lg box-border ";
+    let buttonClass = "answer-button w-full p-4 text-white rounded-lg box-border flex items-center relative ";
     
     if (showResult) {
         if (isCorrect) {
@@ -10,7 +10,7 @@ function AnswerButton({ character, isCorrect, isSelected, onClick, showResult })
             buttonClass += "bg-blue-500";
         }
     } else {
-        buttonClass += "bg-blue-500 hover:bg-blue-600";
+        buttonClass += "bg-blue-500";
     }
 
     return (
@@ -20,7 +20,19 @@ function AnswerButton({ character, isCorrect, isSelected, onClick, showResult })
             onClick={onClick}
             disabled={showResult}
         >
-            {character}
+            <span className="flex-1 text-center">{character}</span>
+            {character.toLowerCase() === 'jerry' && 
+                <img src="character-images/jerry.webp" alt="Jerry" className="h-full w-auto absolute right-2" />
+            }
+            {character.toLowerCase() === 'george' && 
+                <img src="character-images/george.webp" alt="George" className="h-full w-auto absolute right-2" />
+            }
+            {character.toLowerCase() === 'elaine' && 
+                <img src="character-images/elaine.webp" alt="Elaine" className="h-full w-auto absolute right-2" />
+            }
+            {character.toLowerCase() === 'kramer' && 
+                <img src="character-images/kramer.webp" alt="Kramer" className="h-full w-auto absolute right-2" />
+            }
         </button>
     );
 }
