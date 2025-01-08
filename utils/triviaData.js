bfb94e134274998049f78a65f30d9a0ec9d6421b
Justin Carboneau <justin.carboneau@carrot.co>
2025-01-08 17:52:19 -0500

    fix daily find issue

--- utils/triviaData.js
+++ utils/triviaData.js
@@ -19,7 +19,7 @@ async function fetchProduct() {
         const [m, d, y] = date.split(',')[0].split('/');
         const formattedDate = `${y.trim()}-${m.padStart(2, '0')}-${d.padStart(2, '0')}`;
 
-        const response = await fetch(`../products-of-the-day.json`);
+        const response = await fetch(`./products-of-the-day.json`);
         const data = await response.json();
         return data[formattedDate];
     } catch (error) {
